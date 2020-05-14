98aeeba53e53eac56d6106605d8e53f0e3d28ce0
libaihan <http://10.166.20.106/fosunblockchain/bc-game.git>
2020-05-14 16:57:18 +0800

    main函数默认启动线程

--- concurrent/src/com/jianxian/lock/concurrent/QueryMainThread.java
+++ concurrent/src/com/jianxian/lock/concurrent/QueryMainThread.java
@@ -5,6 +5,14 @@ import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 
 public class QueryMainThread {
+    /**
+     * [6] Monitor Ctrl-Break
+     * [5] Attach Listener
+     * [4] Signal Dispatcher      // 分发处理发送给JVM信号的线程
+     * [3] Finalizer              // 调用对象finalizer方法的线程 : 专门用来收集被finalize()方法的队列
+     * [2] Reference Handler      // 清楚Reference的线程 引用
+     * [1] main                   // main函数主线程
+     */
 
     public static void main(String[] args) {
         ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
